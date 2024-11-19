4504a824e47a514fab08b78ba4014c3bda3b3d03
ApBin <apbin@foxmail.com>
2024-11-20 00:18:18 +0800

    三数之和

--- LC15/test.cpp
+++ LC15/test.cpp
@@ -1,25 +1,28 @@
 class Solution {
 public:
-    vector<vector<int>> threeSum(vector<int>& nums) {
-        vector<int> ret;
-        sort(nums.begin(), nums.end());
-        for(int i = 0; i < nums.size();)
-        {
-            if(nums[i] > 0 ) break;
-            int left = 0, right = nums.size()-1, talget = -nums[i];
-            int tmp = (nums[left]+nums[right]);
-            if(tmp > talget) right--;
-            else if(tmp < talget) left++;
-            else {
-                ret.push_back({nums[i], nums{left}, nums[right]});
-                right--, left++;
-                while(left < right && nums[right] == nums[right+1]) right--;
-                while(left < right && nums[left] == nums[left-1]) left++;
-            }
-            i++;
-            while( i < nums.size() && nums[i] == nums[i-1]) i++;
-        }
-        return ret;
-    }
-
+	vector<vector<int>> threeSum(vector<int>& nums) {
+		vector<vector<int>> ret;
+		sort(nums.begin(), nums.end());
+		for (int i = 0; i < nums.size();)
+		{
+			if (nums[i] > 0) break;
+			int left = i + 1, right = nums.size() - 1, talget = -nums[i];
+			while (left < right)
+			{
+				int tmp = (nums[left] + nums[right]);
+				if (tmp > talget) right--;
+				else if (tmp < talget) left++;
+				else
+				{
+					ret.push_back({ nums[i], nums[left], nums[right] });
+					right--, left++;
+					while (left < right && nums[right] == nums[right + 1]) right--;
+					while (left < right && nums[left] == nums[left - 1]) left++;
+				}
+			}
+			i++;
+			while (i < nums.size() && nums[i] == nums[i - 1]) i++;
+		}
+		return ret;
+	}
 };
